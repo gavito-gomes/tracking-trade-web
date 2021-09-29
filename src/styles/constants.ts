2ac683667b49e4448ba5063f2cf0e13ba904ff73
Gabriel Victor <gavitogomes55@gmail.com>
2021-09-29 18:34:31 -0300

    Initial structure

---
+++ src/styles/constants.ts
@@ -0,0 +1,26 @@
+export const screen = {
+  sm: 'min-width: 576px',
+  md: 'min-width: 768px',
+  lg: 'min-width: 1024px',
+  xl: 'min-width: 1440px',
+}
+
+export const colors = {
+  white: '#FAFAFA',
+  light: '#EBEBEB',
+  gray: '#C4C4C4',
+  darkgray: '#B0B0B0',
+  dark: '#333333',
+  lightgray: '#BDBDBD',
+  primary: '#0794B2',
+
+  gradient1: '#0DB2D6',
+  gradient2: '#4FDA91',
+
+  blue: '#4B9EEA',
+  red: '#C00808',
+  lilac: '#A91B79',
+  green: '#7FC008',
+  orange: '#DB8C28',
+  pink: '#EB5757',
+}
